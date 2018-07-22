4e634fa14ed79c94f126baea505e29f84f798fbf
Unknown <glynnisthomas@gmail.com>
2018-07-22 15:08:02 -0500

    next section

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -8,7 +8,7 @@ class QuizQuestion extends Component {
                     </section>
                     <section className="buttons">
                         <ul>
-                            <li>{this.props.quiz_question.answer_options[0]}</li>
+                            <li><QuizQuestion button_text={this.props.quiz_question.answer_options[0]}/></li>
                         </ul>
                     </section>
                 </main>
